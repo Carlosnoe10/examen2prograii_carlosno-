00e0442732d2ffb949e46ada407416f23971135d
Carlosnoe10 <carlosdnoem@gmail.com>
2023-03-24 19:32:58 -0700

    quinto commit bueno se me quito el dolor de cabeza pero ahora me duele el estomago y cago agua pero ni modo a y ya casi termino jeje

--- src/examen2prograii_carlosnoé/Deporte.java
+++ src/examen2prograii_carlosnoé/Deporte.java
@@ -3,10 +3,12 @@ package examen2prograii_carlosnoé;
 import java.util.ArrayList;
 
 public class Deporte {
+    public String Nombre;
     public ArrayList <Torneo> QQDP;
 
-    public Deporte(ArrayList<Torneo> QQDP) {
-        this.QQDP = QQDP;      
+    public Deporte(ArrayList<Torneo> QQDP, String Nombre) {
+        this.QQDP = QQDP;
+        this.Nombre=Nombre;
     }
 
     public ArrayList<Torneo> getQQDP() {
@@ -15,5 +17,14 @@ public class Deporte {
 
     public void setQQDP(ArrayList<Torneo> QQDP) {
         this.QQDP = QQDP;
-    }    
+    }  
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
 }
--- src/examen2prograii_carlosnoé/Principality.java
+++ src/examen2prograii_carlosnoé/Principality.java
@@ -1,10 +1,18 @@
 package examen2prograii_carlosnoé;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
 public class Principality extends javax.swing.JFrame {
 
-    static ArrayList<Torneo> Equipos;
+    static ArrayList<Deporte> Sport;
+    static Object objetoS;
 
     public void ArActu() {
         javax.swing.tree.DefaultMutableTreeNode treeNode0 = new javax.swing.tree.DefaultMutableTreeNode("Torneos");
@@ -18,26 +26,43 @@ public class Principality extends javax.swing.JFrame {
         javax.swing.tree.DefaultMutableTreeNode treeNode5 = new javax.swing.tree.DefaultMutableTreeNode("Q5");
         treeNode0.add(treeNode5);
 
-        for (Torneo MODS : Equipos) {
-            if (MODS.getPeriodoRZ().equalsIgnoreCase("Q1")) {
-                javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
-                treeNode1.add(treeNode);
-            } else if (MODS.getPeriodoRZ().equalsIgnoreCase("Q2")) {
-                javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
-                treeNode2.add(treeNode);
-            } else if (MODS.getPeriodoRZ().equalsIgnoreCase("Q4")) {
-                javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
-                treeNode4.add(treeNode);
-            } else if (MODS.getPeriodoRZ().equalsIgnoreCase("Q5")) {
-                javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
-                treeNode5.add(treeNode);
+        for (Deporte MODS : Sport) {
+            for (int i = 0; i < MODS.getQQDP().size(); i++) {
+                if (MODS.getQQDP().get(i).getPeriodoRZ().equalsIgnoreCase("Q1")) {
+                    javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
+                    treeNode1.add(treeNode);
+                    javax.swing.tree.DefaultMutableTreeNode treeNode6 = new javax.swing.tree.DefaultMutableTreeNode(MODS.getQQDP().get(i).getNombre());
+                    treeNode.add(treeNode6);
+                } else if (MODS.getQQDP().get(i).getPeriodoRZ().equalsIgnoreCase("Q2")) {
+                    javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
+                    treeNode2.add(treeNode);
+                    javax.swing.tree.DefaultMutableTreeNode treeNode6 = new javax.swing.tree.DefaultMutableTreeNode(MODS.getQQDP().get(i).getNombre());
+                    treeNode.add(treeNode6);
+                } else if (MODS.getQQDP().get(i).getPeriodoRZ().equalsIgnoreCase("Q4")) {
+                    javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
+                    treeNode4.add(treeNode);
+                    javax.swing.tree.DefaultMutableTreeNode treeNode6 = new javax.swing.tree.DefaultMutableTreeNode(MODS.getQQDP().get(i).getNombre());
+                    treeNode.add(treeNode6);
+                } else if (MODS.getQQDP().get(i).getPeriodoRZ().equalsIgnoreCase("Q5")) {
+                    javax.swing.tree.DefaultMutableTreeNode treeNode = new javax.swing.tree.DefaultMutableTreeNode(MODS.getNombre());
+                    treeNode5.add(treeNode);
+                    javax.swing.tree.DefaultMutableTreeNode treeNode6 = new javax.swing.tree.DefaultMutableTreeNode(MODS.getQQDP().get(i).getNombre());
+                    treeNode.add(treeNode6);
+                }
             }
-
         }
+    }
+
+    public void NEWJLIST() {
+
     }
 
     public Principality() {
         initComponents();
+        FramePPrincipality.setVisible(true);
+        FramePPrincipality.setLocationRelativeTo(BB);
+        FramePPrincipality.setSize(1250, 680);
+        ArActu();
     }
 
     /**
@@ -54,33 +79,106 @@ public class Principality extends javax.swing.JFrame {
         TablasDeDeporte = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         JtreeDPs = new javax.swing.JTree();
+        jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jFileChooser1 = new javax.swing.JFileChooser();
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        LISTARS = new javax.swing.JMenuItem();
+        ShowTABLASPOS = new javax.swing.JMenuItem();
+        jDialog1 = new javax.swing.JDialog();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
 
+        BB.setBackground(new java.awt.Color(255, 255, 255));
         BB.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        TablasDeDeporte.setBackground(new java.awt.Color(0, 0, 0));
+
+        JtreeDPs.setBackground(new java.awt.Color(255, 255, 255));
+        JtreeDPs.setForeground(new java.awt.Color(0, 0, 0));
+        JtreeDPs.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JtreeDPsMouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                JtreeDPsMousePressed(evt);
+            }
+        });
         jScrollPane1.setViewportView(JtreeDPs);
 
         javax.swing.GroupLayout TablasDeDeporteLayout = new javax.swing.GroupLayout(TablasDeDeporte);
         TablasDeDeporte.setLayout(TablasDeDeporteLayout);
         TablasDeDeporteLayout.setHorizontalGroup(
             TablasDeDeporteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE)
+            .addGroup(TablasDeDeporteLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 405, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(83, Short.MAX_VALUE))
         );
         TablasDeDeporteLayout.setVerticalGroup(
             TablasDeDeporteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
+            .addGroup(TablasDeDeporteLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 449, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(189, Short.MAX_VALUE))
         );
 
-        BB.add(TablasDeDeporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 470, 640));
+        BB.add(TablasDeDeporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 650));
+
+        jButton3.setBackground(new java.awt.Color(255, 51, 51));
+        jButton3.setText("PRESS");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        BB.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 60, 420, 140));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("ADMIN TORNEOS UNITEC");
+        BB.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 0, 400, 70));
+
+        jProgressBar1.setBackground(new java.awt.Color(255, 0, 51));
+        BB.add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 560, 420, 40));
 
         javax.swing.GroupLayout FramePPrincipalityLayout = new javax.swing.GroupLayout(FramePPrincipality.getContentPane());
         FramePPrincipality.getContentPane().setLayout(FramePPrincipalityLayout);
         FramePPrincipalityLayout.setHorizontalGroup(
             FramePPrincipalityLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(BB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(BB, javax.swing.GroupLayout.DEFAULT_SIZE, 1250, Short.MAX_VALUE)
         );
         FramePPrincipalityLayout.setVerticalGroup(
             FramePPrincipalityLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(BB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        LISTARS.setText("ListarEquipos");
+        jPopupMenu1.add(LISTARS);
+
+        ShowTABLASPOS.setText("jMenuItem2");
+        jPopupMenu1.add(ShowTABLASPOS);
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(jList1);
+
+        javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
+        jDialog1.getContentPane().setLayout(jDialog1Layout);
+        jDialog1Layout.setHorizontalGroup(
+            jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jDialog1Layout.createSequentialGroup()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 378, Short.MAX_VALUE))
+        );
+        jDialog1Layout.setVerticalGroup(
+            jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 390, Short.MAX_VALUE)
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -99,9 +197,44 @@ public class Principality extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        jFileChooser1.setFileFilter(new FileNameExtensionFilter("Archivos binarios (*.bin)", "bin"));
+        int seleccion = jFileChooser1.showOpenDialog(BB);
+        if (seleccion == jFileChooser1.APPROVE_OPTION) {
+            File archivoSeleccionado = jFileChooser1.getSelectedFile();
+            String rutaArchivo = archivoSeleccionado.getAbsolutePath();
+            try ( FileInputStream fis = new FileInputStream(rutaArchivo)) {
+                // Leer el archivo binario
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void JtreeDPsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JtreeDPsMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JtreeDPsMouseClicked
+
+    private void JtreeDPsMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JtreeDPsMousePressed
+        if (SwingUtilities.isRightMouseButton(evt)) {
+            // Seleccionar el nodo que se encuentra en la posición del clic
+            int row = JtreeDPs.getClosestRowForLocation(evt.getX(), evt.getY());
+            JtreeDPs.setSelectionRow(row);
+            TreePath path = JtreeDPs.getPathForLocation(evt.getX(), evt.getY());
+            if (path != null) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
+                // Obtener el objeto que representa al nodo
+                Object nodeObject = node.getUserObject();
+                // Hacer algo con el objeto obtenido
+                System.out.println("Datos del nodo seleccionado: " + nodeObject.toString());
+            }
+
+            // Mostrar el JPopupMenu en la posición del clic
+            jPopupMenu1.show(JtreeDPs, evt.getX(), evt.getY());
+
+        }
+    }//GEN-LAST:event_JtreeDPsMousePressed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -129,7 +262,7 @@ public class Principality extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principality().setVisible(true);
+                new Principality();
             }
         });
     }
@@ -138,7 +271,17 @@ public class Principality extends javax.swing.JFrame {
     private javax.swing.JPanel BB;
     private javax.swing.JFrame FramePPrincipality;
     private javax.swing.JTree JtreeDPs;
+    private javax.swing.JMenuItem LISTARS;
+    private javax.swing.JMenuItem ShowTABLASPOS;
     private javax.swing.JPanel TablasDeDeporte;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JDialog jDialog1;
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JPopupMenu jPopupMenu1;
+    private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
